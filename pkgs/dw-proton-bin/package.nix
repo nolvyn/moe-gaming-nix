43d02cbff574ea5c9e3ec07f2e3312581a5d9eee
nolvyn <245221879+nolvyn@users.noreply.github.com>
2026-05-19 04:06:46 +0000

    dw-proton-bin: update to latest

--- pkgs/dw-proton-bin/package.nix
+++ pkgs/dw-proton-bin/package.nix
@@ -6,11 +6,11 @@
 }:
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "dw-proton-bin";
-  version = "11.0-1";
+  version = "11.0-2";
 
   src = fetchzip {
     url = "https://dawn.wine/dawn-winery/dwproton/releases/download/dwproton-${finalAttrs.version}/dwproton-${finalAttrs.version}-x86_64.tar.xz";
-    hash = "sha256-G8VeHp0POZBHfxN78PCwwm6z7zVNMveAdwf/IMJQ/9Q=";
+    hash = "sha256-3AxBr8fQh4bbAsdSSZuyQR2GOz78vdhC6jJbLmiYEXY=";
   };
 
   dontUnpack = true;
